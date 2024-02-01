a66f39661bca74285549755ab71cc7f719a5a060
Robert <robertkey@mighty.co.za>
2024-02-01 13:32:48 +0200

    added code to check for multiple mounts and umount the wrong ones

--- mountdevice.pl
+++ mountdevice.pl
@@ -26,6 +26,9 @@ sub mountdevice {
 
 	# return codes
 	my $rc;
+
+	# flag for indicating if a correct mount is found
+	my $correctmountpoint = "false";
 	
 	#@target = ("TARGET", mountpoint)
 	# check if device is mounted
@@ -42,16 +45,36 @@ sub mountdevice {
 				print "$label is mounted at $mtpt\n";
 				return;
 			} else {
-				# mounted in wrong place
+				# mounted in wrong place and only mounted once
 				# un mount and remount
 				$rc = system("umount $target[0]");
 				die "Could not umount $label at $target[0]: $!\n" unless $rc == 0;
 				# device is now un mounted
 				print "umounted $label from $target[0]\n";
 			}
-		} # end of if @target == 1
+		} else {
+			# @target > 1
+			# umount all wrong mount points
+			# mark if one of the mounts is correct
+			foreach my $item (@target) {
+				if ("$item" ne "$mtpt") {
+					# wrong mount point, umount it
+					$rc = system("umount $item");
+					die "Could not umount $label from $item: $!\n" unless $rc == 0;
+				} else {
+					# correct mount point
+					# set a flag to indicate
+					# a mount point is correct
+					$correctmountpoint = "true";
+				}
+			}
+		} # end of if target == 1
 
-	}
+	} # end of if target
+
+	# if a correct mount point was found , return.
+	# all other mount points would have been umounted
+	return if "$correctmountpoint" eq "true";
 
 	# now mount the device and make directory if it does not exist
 	mkdir $mtpt unless -d $mtpt;
